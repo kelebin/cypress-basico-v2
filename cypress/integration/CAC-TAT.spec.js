e4b6529af4fb2214467e94597003fd1316b12661
Kevin Ferreira <kevinandrade_10@hotmail.com>
2022-09-05 09:47:35 -0300

    first tests with cypress

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Central de atendimento ao Cliente TAT', function() {
+
+    it('verifica o titulo da aplicacao', function(){
+        cy.visit('./src/index.html');
+
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+})
+
+
